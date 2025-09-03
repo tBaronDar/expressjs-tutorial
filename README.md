acd681748dc8415a0a39140b85179832e5497452
tBaronDar <thedarelis@gmail.com>
2025-09-03 18:32:07 +0300

    created some routes

---
+++ README.md
@@ -0,0 +1,33 @@
+# This project includes exercises and notes for Express.js
+
+---
+
+## Notes
+
+### Initializing a project
+
+There is no Vite.js for express.js project the project must be initialized manually.
+
+```bash
+npm init -y
+```
+
+Then install Express.js:
+
+```bash
+npm i express
+```
+
+To run the server on watch mode:
+
+```bash
+npm i -D nodemon
+```
+
+"-D" means "developer dependency". Also we can use:
+
+```bash
+node --watch server.js
+```
+
+It does the same thing but nodemon can be customizised
---
+++ src/index.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import { env } from "node:process";
+
+// Create an express application
+const app = express();
+
+const PORT = env.PORT || 3000;
+console.log(env.PORT);
+
+// Listen on selected port
+// the second argument is a callback function that is called when the server has started
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+// Define a route handler for the root path
+// The first argument is the path, the second argument is a callback function that is called when the route is matched
+// The callback function takes two arguments: the request object and the response object
+// The request object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on.
+// The response object represents the HTTP response that an Express app sends when it gets an HTTP request.
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+//an other example
+//manually set the status code to 200
+app.get("/api/users", (req, res) => {
+	res.status(200).send([
+		{ id: 1, name: "John" },
+		{ id: 2, name: "Jane" },
+		{ id: 3, name: "Jim" },
+	]);
+});
